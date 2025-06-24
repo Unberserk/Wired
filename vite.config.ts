48250e0a279a9451b56bf5628d1c42a480ec8cc6
Unberserk <104165736+Unberserk@users.noreply.github.com>
2025-06-23 20:49:17 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
+    base: '/',
     plugins: [react()], // Using React plugin from Vite
 
     build: {
